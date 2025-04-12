419c9fdee2b4b1334fe35f92d86aa4bf60303e94
Alexander Luna <alexanderfluna@outlook.com>
2025-04-12 08:24:50 -0400

    Updates

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <img className="comnet-logo" src="/images/Comnet-Logo.jpg" alt="Comnet Logo"/>
+
+      <div>
+
+      </div>
+      <div></div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Home = () => {
+  return (
+    <main>
+        <Navbar/>
+        <div>Home</div>
+    </main>
+  )
+}
+
+export default Home
